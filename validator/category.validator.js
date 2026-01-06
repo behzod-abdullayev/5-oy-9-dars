0d1539398a485880726cb491ee1508fcb64de7c4
behzod07-abdullayev <bekzod2366@gmail.com>
2026-01-07 01:47:14 +0500

    5-oy-9-dars

--- validator/category.validator.js
+++ validator/category.validator.js
@@ -6,7 +6,19 @@ const categoryValidator = joi.object({
     "string.min": "Kategoriya nomi kamida 2 ta harf bo'lishi kerak",
     "any.required": "Kategoriya nomini kiritish shart",
   }),
-
-  image: joi.string().uri().allow("", null),
+  image: joi.string().required().messages({
+    "any.required": "Brend logotipi (rasmi) bo'lishi shart",
+  }),
+  foundedYear: joi.number()
+    .integer()
+    .max(new Date().getFullYear())
+    .messages({
+      "number.max": "Tashkil etilgan yil hozirgi yildan katta bo'lishi mumkin emas"
+    }),
+  founder: joi.string().trim().required().messages({
+    "any.required": "Asoschi ismini kiritish shart"
+  }),
+  description: joi.string().min(3).max(150).trim()
 });
+
 module.exports = categoryValidator;
